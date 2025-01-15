3d703a8bf368b130c7a552230e24c87c413d6ab4
rafaelMachado1891 <rafadmn1@gmail.com>
2025-01-15 12:13:44 -0300

    iniciei a analise exploratória dos dados

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import pandas as pd
+caminho = 'C:/Users/PCP/Documents/repositorios_git/predicao_estoque/movimento_estoque.csv'
+
+df = pd.read_csv(caminho, delimiter=';')
+
+df['MOVIMENTO'] = pd.to_numeric(df['MOVIMENTO'], errors='coerce')
+
+media_consumo = df.groupby(['Codigo'])['MOVIMENTO'].mean()
+
+print(media_consumo)

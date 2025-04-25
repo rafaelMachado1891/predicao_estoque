d6e453cba1da8a93e1b65c8b00ca6f3780689ab9
rafaelMachado1891 <rafadmn1@gmail.com>
2025-04-25 15:56:08 -0300

    update

--- src/app.py
+++ src/app.py
@@ -1,14 +1,52 @@
 import pandas as pd
 
 caminho = pd.read_csv("../movimento_estoque.csv",sep=";")
+produto_terceiro = pd.read_csv("../produto_terceiros.csv",sep=";")
 
+df_prod= pd.DataFrame(produto_terceiro)
 df = pd.DataFrame(caminho)
 
-filtro = df["Codigo"] == 577
+colunas = ['Codigo', "Descricao", "MES", "ANO", "TOTAL_MOVIMENTO"]
+colunas_prod = ["Codigo", "Estoque_Minimo", "Cod_Terc"]
 
-df = df[filtro]
+df_prod=df_prod[colunas_prod]
+df=df[colunas]
 
-# Certifica-se de que a coluna 'Total_Movimento' está numérica
-df["Total_Movimento"] = pd.to_numeric(df["TOTAL_MOVIMENTO"], errors="coerce")
+rename_columns={"Codigo": "codigo", "Descricao":"descricao","MES": "mes", "ANO": "ano", "TOTAL_MOVIMENTO": "quantidade"}
+rename_columns_prod={"Codigo": "codigo", "Estoque_Minimo": "estoque", "Cod_Terc": "terceiro"}
 
-print(df["TOTAL_MOVIMENTO"].describe())
+df.rename(columns=rename_columns,inplace=True)
+df_prod.rename(columns=rename_columns_prod, inplace=True)
+
+tipo_dados_prod = {
+    "codigo": str,
+    "estoque": int,
+    "terceiro": int
+}
+
+print(df_prod)
+df_prod = df_prod.astype(tipo_dados_prod)
+
+df["quantidade"] = df["quantidade"].str.replace(",",".").astype(float)
+
+tipo_dados = {
+    "codigo": str,
+    "descricao": str,
+    "ano": int,
+    "mes": int,
+    "quantidade": float
+}
+
+df = df.astype(tipo_dados)
+
+df=df.groupby(by=["codigo", "descricao","mes", "ano"],as_index=False).agg({"quantidade":["sum"]})
+
+df.columns = ["codigo", "descricao", "mes", "ano", "quantidade_sum"]
+
+print(df)
+
+agregattions = df.groupby(by=["codigo", "descricao"],as_index=False).agg({"quantidade_sum":["sum", "min", "max", "mean", "std", "var", "count"]})
+
+print(agregattions)
+
+agregattions.to_csv("calculo.csv",index=False)

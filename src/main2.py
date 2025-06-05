91991ac356468eef83441481b6d51e7fe35555b1
rafaelMachado1891 <rafadmn1@gmail.com>
2025-06-05 17:07:16 -0300

    atualizacao dos calculos de estoque

--- src/main2.py
+++ src/main2.py
@@ -26,19 +26,36 @@ tipo_dados = {
 
 df = df.astype(tipo_dados)
 
+df["data"] = pd.to_datetime(df["data"], dayfirst=True, errors="coerce")
+
+data_consulta = input("a partir de qual data você deseja consultar? digite no formato(ano/mes/dia): ")
+
+data_consulta = pd.to_datetime(data_consulta, format="%Y/%m/%d", errors="coerce")
+
+df = df.loc[df["data"]>= data_consulta]
+
 agregate = df.groupby(by=["codigo", "descricao"],as_index=False).agg({"quantidade":["sum","min","max","var","mean","std","count"]})
 
 agregate.columns = ['_'.join(col).strip('_') for col in agregate.columns.values]
 
 df = agregate
 
-df["calculo_estoque"] = round(df["quantidade_mean"] + (2 * df["quantidade_std"]),0)
+df["calculo_estoque"] = round(df["quantidade_mean"] + (3 * df["quantidade_std"]),0)
 
-df["amplitude"] = df["quantidade_min"] - df["quantidade_max"]
+df["amplitude"] = df["quantidade_max"] - df["quantidade_min"]
 
-selecao = ["codigo", "descricao", "quantidade_sum", "quantidade_mean", "calculo_estoque", "quantidade_count","quantidade_min","quantidade_max" ,"quantidade_var", "amplitude","quantidade_std"]
+selecao = ["codigo", "descricao", "quantidade_sum", "quantidade_mean", "calculo_estoque","quantidade_min","quantidade_max" ,"quantidade_var", "amplitude","quantidade_std", "quantidade_count"]
 
-df=df[selecao]
+df = df[selecao].copy()
+
+total = df["quantidade_count"].sum()
+
+df["frequencia_relativa"] = round(df["quantidade_count"] / total, 4 )
+
+nome = { "quantidade_sum": "soma", "quantidade_mean": "media", "calculo_estoque": "calculo", "quantidade_min": "minimo", "quantidade_max": "maximo", "quantidade_var": "variancia", 
+         "quantidade_std": "desvio_padrao", "quantidade_count": "frequencia"}
+
+df.rename(columns=nome, inplace= True)
 
 df.to_csv("correios.csv",index=False)
 

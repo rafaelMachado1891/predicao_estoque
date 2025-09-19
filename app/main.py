db548f81c2718deb8f10a42c1007215f2af4124f
rafaelMachado1891 <rafadmn1@gmail.com>
2025-09-19 16:09:21 -0300

    update

--- app/main.py
+++ app/main.py
@@ -10,13 +10,13 @@ df['Data_EM'] = pd.to_datetime(df['Data_EM'], errors='coerce')
 df['trimestre'] = df['Data_EM'].dt.quarter
 df['dia_semana'] = df['Data_EM'].dt.day_of_week
 
-colunas = ["Numero", "Data_EM", "Codigo", "Descricao", "Quantidade", "Pedido", "MARCA", "GRUPO", "NUMERO_SEMANA", "ANO", "trimestre", "dia_semana"]
+colunas = ["Numero", "Data_EM", "Codigo", "Descricao", "Quantidade", "Pedido", "MARCA", "GRUPO", "NUMERO_SEMANA", "ANO", "trimestre", "dia_semana", "MES", "MES-ANO"]
 
 df = df[colunas]
 
 rename_columns = {
     "Numero": "numero", "Data_EM": "data", "Codigo": "codigo", "Descricao": "descricao",
-    "Quantidade": "quantidade", "Pedido": "pedido", "MARCA": "marca", "GRUPO": "grupo", "NUMERO_SEMANA": "numero_semana", "ANO": "ano"
+    "Quantidade": "quantidade", "Pedido": "pedido", "MARCA": "marca", "GRUPO": "grupo", "NUMERO_SEMANA": "numero_semana", "ANO": "ano","MES-ANO": "mes-ano","MES": "mes" 
 }
 
 df = df.rename(columns=rename_columns)
@@ -30,6 +30,8 @@ df = df.astype({
     "marca": str,
     "grupo": str,
     "numero_semana": int,
+    "mes": int,
+    "mes-ano": str,
     "ano": int, 
     "trimestre": int,
     "dia_semana": int    
@@ -39,17 +41,21 @@ df = df.astype({
 
 media_diaria = df
 
-media_diaria = media_diaria.groupby(["codigo", "data"],as_index=False).aggregate({"quantidade": "sum",
-                                                                                  "descricao": "first",
-                                                                                  "marca": "first",
-                                                                                  "grupo": "first"
-                                                                                  })
+media_diaria = media_diaria.groupby(["codigo", "descricao"],as_index=False).aggregate({"quantidade": ["sum", "mean","count"],                                                                                       
+                                                                                        "descricao": "first",
+                                                                                        "marca": "first",
+                                                                                        "grupo": "first"
+                                                                                        })
 
 
 media_mensal = df
 
-media_mensal = media_mensal.groupby(["codigo", "ano", "mes"],as_index=False).aggregate({"quantiade": "sum",
-                                                                                        "quantidade": "mean"})
+media_mensal = media_mensal.groupby(["codigo", "descricao", "ano", "mes"],as_index=False).aggregate({"quantidade": ["sum", "mean","count"],                                                                                       
+                                                                                        "descricao": "first",
+                                                                                        "marca": "first",
+                                                                                        "grupo": "first"
+                                                                                        })
 
-print(media_mensal)
+print(media_diaria)
+
 # media_diaria = media_diaria.aggregate()
